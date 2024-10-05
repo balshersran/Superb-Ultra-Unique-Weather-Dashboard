2a97c5100ca986c1f70335396c1188c15970a272
Balsher Sran <balshersran@Balshers-MacBook-Pro.local>
2024-10-05 14:59:57 -0700

    added interface and defined the fetchdata method

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -2,26 +2,38 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
+interface Coordinates {
+  lon: number;
+  lat: number;
+}
 
 // TODO: Define a class for the Weather object
 class Weather {
+
+  city: string;
+  icon: string;
+  iconDescription: string;
+  date: number;
+  tempF: number;
+  windSpeed: number;
+  humidity: number;
+
   constructor(
-    city: string, 
-    date: number, 
-    icon: any, 
-    iconDescription: string, 
-    tempF: number, 
+    city: string,
+    icon: string,
+    iconDescription: string,
+    date: number,
+    tempF: number,
     windSpeed: number,
-    humidity: number)
-    {
-      this.city = city;
-      this.date = date;
-      this.icon = icon;
-      this.iconDescription = iconDescription;
-      this.tempF = tempF;
-      this.windSpeed = windSpeed;
-      this.humidity = humidity;
-    }
+    humidity: number) {
+    this.city = city;
+    this.icon = icon;
+    this.iconDescription = iconDescription;
+    this.date = date;
+    this.tempF = tempF;
+    this.windSpeed = windSpeed;
+    this.humidity = humidity;
+  }
 }
 
 
@@ -32,13 +44,25 @@ class WeatherService {
 
   private apikey?: string;
 
+  cityName: string;
+
   constructor() {
     this.baseUrl = process.env.API_BASE_URL || '';
 
     this.apikey = process.env.API_KEY || '';
+
+    this.cityName: '';
   }
   // TODO: Create fetchLocationData method
-  // private async fetchLocationData(query: string) {}
+  private async fetchLocationData(query: string) {
+    try {
+      const response = await fetch(query);
+      const locationData = await response.json();
+      return locationData;
+    } catch (err) {
+      console.log('Error:', err);
+    }
+  }
   // TODO: Create destructureLocationData method
   // private destructureLocationData(locationData: Coordinates): Coordinates {}
   // TODO: Create buildGeocodeQuery method
